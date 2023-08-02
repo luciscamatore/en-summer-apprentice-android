58ccbb08992191be40a06e02a81a83187782e085
luciscamatore <sandel.delu@gmail.com>
2023-08-02 23:50:10 +0300

    search done, update for sort, almost done

--- app/src/main/java/com/endava/tms/MainActivity.java
+++ app/src/main/java/com/endava/tms/MainActivity.java
@@ -21,58 +21,56 @@ import com.endava.tms.model.Event;
 import java.time.LocalDate;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public List<Event> eventList;
-    public EventAdapter eventAdapter;
-    public ArrayAdapter<Event> adapter;
+    public EventAdapter sortAdapter;
+    public EventAdapter searchAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setUpSearch();
-        setUpSpinner();
 
         this.setTitle("Events");
         //getSupportActionBar().hide();
 
+        setUpSearch();
+        setUpSpinner();
+        fillList();
+        setUpReciclerView();
+
+
+    }
+    public void setUpReciclerView(){
         RecyclerView recyclerView = findViewById(R.id.reciclerview);
-
-
-        eventList = new ArrayList<>();
-        eventList.add(new Event("Electric Castle","Muzica electronica si nu numai","Bontida Castle","Festival", LocalDate.now(), LocalDate.now(),R.drawable.ec));
-        eventList.add(new Event("Untold","Muzica electronica si nu numai","la untold","Festival", LocalDate.now(), LocalDate.now(),R.drawable.untold));
-        eventList.add(new Event("Wine Festival","Vin","Central Park Cluj-Napoca","Beutura", LocalDate.now(), LocalDate.now(),R.drawable.wine));
-        eventList.add(new Event("Maieru vs Sangeorz","Folbal","Sintetic Sangeorz","Sport", LocalDate.now(), LocalDate.now(),R.drawable.fb));
-        eventList.add(new Event("SYF","Sangeorz Youth Fest","Sangeorz-Bai Central Park","Muzica", LocalDate.now(), LocalDate.now(),R.drawable.syf));
-
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-//        recyclerView.setAdapter(new EventAdapter(getApplicationContext(),eventList));
+//      recyclerView.setAdapter(new EventAdapter(getApplicationContext(),eventList));
 
-        this.adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, eventList);
+        searchAdapter = new EventAdapter(this, this.eventList);
 
-        this.eventAdapter = new EventAdapter(this, this.eventList);
-        recyclerView.setAdapter(this.eventAdapter);
-
+        recyclerView.setAdapter(this.searchAdapter);
     }
 
     public void setUpSpinner(){
         Spinner spinner = findViewById(R.id.event_spinner);
         ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.event_sort, android.R.layout.simple_spinner_dropdown_item);
+        spinner.setPrompt("Sort by");
         spinner.setAdapter(spinnerAdapter);
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @SuppressLint("NotifyDataSetChanged")
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                if(i == 1) {
-                    eventList.sort((e1, e2) -> e1.getEventName().compareTo(e2.getEventName()));
-                    Log.v("HERE", eventList.toString());
-                }
-                eventAdapter.notifyDataSetChanged();
+                sorter(i);
+                searchAdapter.notifyDataSetChanged();
             }
+
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
 
@@ -82,20 +80,48 @@ public class MainActivity extends AppCompatActivity {
 
     public void setUpSearch(){
         SearchView searchView = findViewById(R.id.searchbar);
-        searchView.setSubmitButtonEnabled(true);
+        //searchView.setSubmitButtonEnabled(true);
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
-                adapter.getFilter().filter(s);
-                Log.v("HERE", s);
                 return false;
             }
             @Override
             public boolean onQueryTextChange(String s) {
-                adapter.getFilter().filter(s);
+                searchAdapter.getFilter().filter(s);
                 return false;
             }
         });
+    }
+
+    public void sorter(int position){
+        switch (position){
+            case 0: //aici trebe dupa pret (vedem cand ne bagam la conectare la backend)
+                eventList.sort((e1, e2) -> e1.getEventType().compareTo(e2.getEventType()));
+                break;
+            case 1: //la fel ca la 0
+                eventList.sort((e1,e2) -> e1.getEventName().compareTo(e2.getEventName()));
+                Collections.reverse(eventList);
+                break;
+            case 2:
+                eventList.sort((e1,e2) -> e1.getEventName().compareTo(e2.getEventName()));
+                break;
+            case 3:
+                eventList.sort(Comparator.comparing(Event::getEventName));
+                Collections.reverse(eventList);
+                break;
+            default:
+                break;
+        }
+    }
+    public void fillList(){
+        eventList = new ArrayList<>();
+        eventList.add(new Event("Electric Castle","Muzica electronica si nu numai","Bontida Castle","Festival", LocalDate.now(), LocalDate.now(),R.drawable.ec));
+        eventList.add(new Event("Untold","Muzica electronica si nu numai","la untold","Festival", LocalDate.now(), LocalDate.now(),R.drawable.untold));
+        eventList.add(new Event("Wine Festival","Vin","Central Park Cluj-Napoca","Beutura", LocalDate.now(), LocalDate.now(),R.drawable.wine));
+        eventList.add(new Event("Maieru vs Sangeorz","Folbal","Sintetic Sangeorz","Sport", LocalDate.now(), LocalDate.now(),R.drawable.fb));
+        eventList.add(new Event("SYF","Sangeorz Youth Fest","Sangeorz-Bai Central Park","Muzica", LocalDate.now(), LocalDate.now(),R.drawable.syf));
+
     }
 }
--- app/src/main/java/com/endava/tms/adapter/EventAdapter.java
+++ app/src/main/java/com/endava/tms/adapter/EventAdapter.java
@@ -1,10 +1,12 @@
 package com.endava.tms.adapter;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -15,17 +17,23 @@ import com.endava.tms.R;
 import com.endava.tms.model.Event;
 import com.endava.tms.viewholder.EventViewHolder;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class EventAdapter extends RecyclerView.Adapter<EventViewHolder> {
     Context context;
     List<Event> events;
+    List<Event> eventsFull;
 
     public EventAdapter(Context context, List<Event> events) {
         this.context = context;
         this.events = events;
+        eventsFull = new ArrayList<>(events);
     }
-
+    public EventAdapter(List<Event> events){
+        this.events = events;
+        eventsFull = new ArrayList<>(events);
+    }
     @NonNull
     @Override
     public EventViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -53,5 +61,36 @@ public class EventAdapter extends RecyclerView.Adapter<EventViewHolder> {
     public int getItemCount() {
         return events.size();
     }
+
+    public Filter getFilter(){
+        return eventFilter;
+    }
+    private Filter eventFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence charSequence) {
+            List<Event> filteredEvents = new ArrayList<>();
+
+            if(charSequence == null || charSequence.length() == 0)
+                filteredEvents.addAll(eventsFull);
+            else{
+                String filterPattern = charSequence.toString().toLowerCase().trim();
+
+                for(Event ev : eventsFull)
+                    if(ev.getEventName().toLowerCase().contains(filterPattern))
+                        filteredEvents.add(ev);
+            }
+            FilterResults results = new FilterResults();
+            results.values = filteredEvents;
+            return results;
+        }
+
+        @SuppressLint("NotifyDataSetChanged")
+        @Override
+        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+            events.clear();
+            events.addAll((List)filterResults.values);
+            notifyDataSetChanged();
+        }
+    };
 
 }
